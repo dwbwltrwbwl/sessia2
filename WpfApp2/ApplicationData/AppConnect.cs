60f6899d35543bed789bb11d1cd468ea03288e52
10220448 <10220448@D205PC25>
2025-09-16 10:36:25 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/ApplicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static sessia2Entities2 model01;
+    }
+}
---
+++ WpfApp2/Pages/Autorization.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp2.ApplicationData;
+
+namespace WpfApp2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorization.xaml
+    /// </summary>
+    public partial class Autorization : Page
+    {
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = AppConnect.model01.users.FirstOrDefault((x => x.login == TBLogin.Text && x.password == PBPassword.Password));
+                if (userObj == null)
+                {
+                    MessageBox.Show("Такого пользователя нет", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    switch (userObj.id_role)
+                    {
+                        case 1:
+                            MessageBox.Show("Здравствуйте, Администратор " + userObj.name + "!",
+                            "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            break;
+                        case 2:
+                            MessageBox.Show("Здравствуйте, Ученик " + userObj.name + "!",
+                            "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            break;
+                        default:
+                            MessageBox.Show("Данные не обнаружены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка " + ex.Message.ToString() + "Критическая ошибка приложения!",
+                    "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
